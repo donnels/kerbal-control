c4fee5d0f37a649a57226158f2e47218b41337c9
Sean Donnellan <github@donnellan.de>
2022-01-30 20:31:49 +0100

    fastled example

--- fastled-test/fastled-test.ino
+++ fastled-test/fastled-test.ino
@@ -7,8 +7,15 @@ CRGB leds[NUM_LEDS];
 
 void setup() { 
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-    leds[16] = CHSV(0, 255, 16);
-    leds[12] = CHSV(128, 255, 16);
+    leds[12] = CHSV(0, 255, 16);
+    leds[13] = CHSV(33, 255, 16);
+    leds[14] = CHSV(65, 255, 16);
+    leds[15] = CHSV(97, 255, 16);
+    leds[16] = CHSV(129, 255, 16);
+    leds[17] = CHSV(161, 255, 16);
+    leds[18] = CHSV(193, 255, 16);
+    leds[19] = CHSV(225, 255, 16);
+    leds[20] = CHSV(255, 255, 16);
     leds[21] = CHSV(255, 255, 16);
     FastLED.show();
 }
