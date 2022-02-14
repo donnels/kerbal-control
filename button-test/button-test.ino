f87c0e71e2b706164e2312b80bcd4cde51a0d0c0
DonFynn <githubf@donnellan.de>
2022-02-14 16:45:33 +0100

    changed button-test1 back to a latching button

--- button-test/button-test.ino
+++ button-test/button-test.ino
@@ -1,39 +1,30 @@
-const int buttonPin = 8;    
-const int ledPin = LED_BUILTIN;    
-//start with led OFF  
-int ledState = LOW;        
-int buttonState; 
-//unpressed is assumd            
-int lastButtonState;   
+const int buttonPin = 9;    
+const int ledPin = 13;      
+int ledState = HIGH;         
+int buttonState;             
+int lastButtonState = LOW;  
 unsigned long lastDebounceTime = 0;  
-unsigned long debounceDelay = 5;   
+unsigned long debounceDelay = 50;   
 
 void setup() {
-  //default unpressed=HIGH
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  Serial.begin(115200);
   digitalWrite(ledPin, ledState);
-  int buttonState = digitalRead(buttonPin);
-  lastButtonState=buttonState;
-  Serial.println("setup complete");
 }
 
 void loop() {
   int reading = digitalRead(buttonPin);
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
-  }
+  } 
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading != buttonState) {
       buttonState = reading;
-      Serial.println("transition High2Low");
-      if (buttonState == LOW ) {
+      if (buttonState == HIGH) {
         ledState = !ledState;
-        Serial.println("transition Low2High");
       }
     }
   }
-  digitalWrite(ledPin, ledState);  
+  digitalWrite(ledPin, ledState);
   lastButtonState = reading;
 }
---
+++ button_test2/button_test2.ino
@@ -0,0 +1,42 @@
+const int buttonPin = 9;    
+const int ledPin = LED_BUILTIN;    
+//start with led OFF  
+int ledState = LOW;        
+int buttonState; 
+//unpressed is assumd            
+int lastButtonState;   
+unsigned long lastDebounceTime = 0;  
+unsigned long debounceDelay = 5;   
+
+void setup() {
+  //default unpressed=HIGH
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  digitalWrite(ledPin, ledState);
+  int buttonState = digitalRead(buttonPin);
+  lastButtonState=buttonState;
+  Serial.println("setup complete");
+  Serial.print("Button is ");
+  Serial.println(buttonState);
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) >= debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      Serial.println("transition");
+    }
+    if (buttonState == LOW ) {
+      ledState = !ledState;
+      Serial.println("transition High2Low");
+    } 
+  }
+  delay(500);
+  digitalWrite(ledPin, ledState);  
+  lastButtonState = reading;
+}
